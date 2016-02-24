2fe7d8dfea56a1213e171c2132a36f6cb114dab7
Demetra Haloutsos <demetra2h@gmail.com>
2016-02-24 10:22:30 -0800

    implement paperclip

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,15 @@
 class Picture < ActiveRecord::Base
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   belongs_to :user
   validates :imgur_id, uniqueness: true, presence: true
   validates :title, presence: true
 
   def imgur_link
     "http://www.imgur.com/#{self.imgur_id}"
-end
+  end
 
   def fragment_id
     "picture-#{self.id}"
